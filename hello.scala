3a04614cf5e72d21383114a02d27974c3eb05ab8
Sylvain PERTHUÉ <sylvain.perthue@etu.univ-poitiers.fr>
2022-12-05 08:50:50 +0000

    1er version

---
+++ hello.scala
@@ -0,0 +1,11 @@
+
+// Scala program to print Hello World! 
+object Hello
+{
+    // Main Method 
+    def main(args: Array[String]) 
+    {
+        // prints Hello World
+        println("Hello World! by " +args(0)+ " "+ args(1) ) 
+    }
+}
